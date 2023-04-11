e19768fbf6075fb491647c4c78b525d07a5c776a
Your Name <you@example.com>
2023-04-11 12:52:12 +0700

    fix image import not working on gh

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,8 @@
-
+import pizza from '../images/pizza.jpg'
+import biryani from '../images/biryani.jpg'
+import salad from '../images/salad.jpg'
+import steak from '../images/steak.jpg'
+import breakfast from '../images/breakfast.jpg'
 
 function createMenu(){
     const menu = document.createElement('section')
@@ -10,11 +14,11 @@ function createMenu(){
 
     const container = document.createElement('div')
     container.classList.add('menu-container')
-    container.appendChild(createMenuItem('Cheezy Pizza','../images/pizza.jpg'))
-    container.appendChild(createMenuItem('Chicken Biryani',  '../images/biryani.jpg'))
-    container.appendChild(createMenuItem('Vagetable Salad', '../images/salad.jpg'))
-    container.appendChild(createMenuItem('Beef Steak', '../images/steak.jpg'))
-    container.appendChild(createMenuItem('Breakfast Set','../images/breakfast.jpg'))
+    container.appendChild(createMenuItem('Cheezy Pizza', pizza))
+    container.appendChild(createMenuItem('Chicken Biryani',  biryani))
+    container.appendChild(createMenuItem('Vagetable Salad', salad))
+    container.appendChild(createMenuItem('Beef Steak', steak))
+    container.appendChild(createMenuItem('Breakfast Set', breakfast))
 
     menu.appendChild(container)
 
